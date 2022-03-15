c8af0c3991b55f518985bc43bedb0f498fd2d337
Michael Simmons <msimmons86@gmail.com>
2022-03-14 19:17:57 -0700

    commit tree 02d9a17fc491106544673496afd30c449e09f477

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Harley Simmons';
+
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
